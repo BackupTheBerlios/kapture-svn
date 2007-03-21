0c4e1c1dec05aeed399a0363eda159531fa7df44
cazou <cazou@5a264973-282a-0410-b1ad-c87cf3b624cc>
2007-03-21 20:42:00 +0000

    - Semicolon missing : corrected

--- trunk/xmlHandler.h
+++ trunk/xmlHandler.h
@@ -17,7 +17,7 @@ public:
 	// MUST be called to set the stream data and to allow the creation of xmlTree.
 	void setData(QByteArray data);
 	stanza s;
-	QString nameSpace // Not used but will certainly be....
+	QString nameSpace; // Not used but will certainly be....
 private:
 	QList<QString> tags; // List al tags received in the stream (check that the stream is valid)
 	QString xmlTree; // The received Stream
